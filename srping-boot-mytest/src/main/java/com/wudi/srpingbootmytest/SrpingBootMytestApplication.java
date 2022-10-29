b1c4a776c836b3e3be32b6fd8a699d5e86911458
laowu1024 <395153528@qq.com>
2022-10-30 01:05:46 +0800

    init my module

---
+++ srping-boot-mytest/src/main/java/com/wudi/srpingbootmytest/SrpingBootMytestApplication.java
@@ -0,0 +1,14 @@
+package com.wudi.srpingbootmytest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingBootMytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingBootMytestApplication.class, args);
+	}
+
+}
